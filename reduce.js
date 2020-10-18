a53ea15b44ae0639bbeb5e4ba6e53acab52fc2d5
Rome Bell <romebell5@gmail.com>
2020-10-18 08:17:51 -0700

    [reduce] Update variable keyword for numbers

--- reduce.js
+++ reduce.js
@@ -1,6 +1,6 @@
 // BONUS ONLY: Look up docs to read about reduce, which takes an array and distills it into a single value.
 
-var numbers = [1, 56, 2, 4, 1, 99, 3, 5];
+const numbers = [1, 56, 2, 4, 1, 99, 3, 5];
 
 // Your code here!
 
