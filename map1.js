d443474e1d8be03518e79725b9627595c6a484cf
Rome Bell <romebell5@gmail.com>
2020-10-18 08:16:31 -0700

    [map1] Update variable keyword

--- map1.js
+++ map1.js
@@ -1,4 +1,4 @@
-var mneumonics = ['My Very Excellent Mother Just Sent Me Nine Pizzas', 'Please Excuse My Dear Aunt Sally', 'Every Good Boy Does Fine'];
+const mneumonics = ['My Very Excellent Mother Just Sent Me Nine Pizzas', 'Please Excuse My Dear Aunt Sally', 'Every Good Boy Does Fine'];
 
 // Write some code using map that takes the above array of mneumonics, and returns a two dimensional array.
 // For example, the 0th element of the new array should be:
