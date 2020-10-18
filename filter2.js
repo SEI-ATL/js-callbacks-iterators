c1ad83bbb71f4ccb36c95facdd430fafe6d6744d
Rome Bell <romebell5@gmail.com>
2020-10-18 08:19:11 -0700

    [filter2] Update variable keyword for misc

--- filter2.js
+++ filter2.js
@@ -1,4 +1,4 @@
-var misc = [3, "true", {a: 1, b: 2}, 7, [1, 2, 3], ['a', 'b', 'c'], "my favorite string", 59.09]
+const misc = [3, "true", {a: 1, b: 2}, 7, [1, 2, 3], ['a', 'b', 'c'], "my favorite string", 59.09]
 
 // Use filter to filter all strings out of the above array. 
 // Don't worry about strings in the nested array (misc[5] is considered an array, not a string)
